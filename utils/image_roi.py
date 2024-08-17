2ef2e12812407f8eafdde4e62b60f445fcef3b66
Mészáros Róbert <mrobert3456@gmail.com>
2024-08-17 10:47:58 +0200

    updated README

--- utils/image_roi.py
+++ utils/image_roi.py
@@ -7,10 +7,12 @@ def get_image_roi(image):
     im_shape = image.shape
 
     vertices = np.array(
-        [[(0, im_shape[0] * .85),
-          (im_shape[1] * .10, im_shape[0] * .35)
-             , (im_shape[1] * .90, im_shape[0] * .35),
-          (im_shape[1], im_shape[0] * .85)]], dtype=np.int32)  # creates an array with the trapezoids verticies
+        [[
+          (0, im_shape[0] * .85),# bottom left
+          (im_shape[1] * .10, im_shape[0] * .35), # top left
+          (im_shape[1] * .90, im_shape[0] * .35), # top right
+          (im_shape[1], im_shape[0] * .85) # bottom right
+        ]], dtype=np.int32)  # creates an array with the trapezoids verticies
 
     cv.fillPoly(mask, vertices, (255,) * 3)
     masked_image = cv.bitwise_and(image, mask)  # crops the original image with the mask
